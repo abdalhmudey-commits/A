df4c8f0baeff1479f375aaf642a8f54858342fb4
عبد السلام <abdalhmudey@gmail.com>
2025-11-23 12:48:10 +0000

    كيف اوافق واين اوافق

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {gemini25Flash} from 'genkitx-google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({
     apiVersion: 'v1beta'
   })],
-  model: gemini25Flash,
+  model: 'gemini-2.5-flash',
 });
