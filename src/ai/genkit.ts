232d18ab862e0bc367234ffe6f160459d5c58e31
عبد السلام <abdalhmudey@gmail.com>
2025-11-23 12:47:33 +0000

    انا احتاج اعاده دفع الملفات الى جيت هوب

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,8 @@ import {googleAI} from '@genkit-ai/google-genai';
 import {gemini25Flash} from 'genkitx-google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiVersion: 'v1beta'
+  })],
   model: gemini25Flash,
 });
