339fb0790d8c5164b908c4be4211d8fc0b8db5ea
عبد السلام <abdalhmudey@gmail.com>
2025-11-23 12:46:24 +0000

    اكتملت عمليه البناء لكن ارجو ان لا يكون ذلك عائقا كبيرا ما الذي في الصور

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {gemini25Flash} from 'genkitx-google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  model: gemini25Flash,
 });
